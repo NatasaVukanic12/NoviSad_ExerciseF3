56e632a1072ad8b312fbd6e6fb944c5f95160f88
somename <someemail>
2017-05-12 20:35:49 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,11 @@
 // Student ID:
 
 public class PlanetExplorer {
+	
+
+	public int x;
+	public int y;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +17,9 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		this.x=0;
+		this.y=0;
+		
 	}
 	
 	public String executeCommand(String command){
@@ -27,5 +35,21 @@ public class PlanetExplorer {
 		 */
 		
 		return null;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 }
